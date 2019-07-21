bc1db6d7f07f0e2035c1681ac4b95c58e1c00a3a
Ty Nelson <tynelson48@yahoo.com>
2019-07-21 15:10:48 -0500

    creating random numbers and assigning them to gems and number to win. works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,58 @@
 $(document).ready(function() {
+
     // define generic variables.
     var wins = 0;
     var losses = 0;
-    var totalScore = 0;
     var randomNumber = 0;
-    var redGemValue = 0;
-    var blueGemValue = 0;
-    var yellowGemValue = 0;
-    var greenGemValue = 0;
+    var gemValue = 0;
+    var totalScore = 0;
+    var gameStart = false;
+
+    // variable to hold values for each gem.
+    var gem = {
+        redGem: {
+            value: 0
+        },
+        blueGem: {
+            value: 0
+        },
+        yellowGem: {
+            value: 0
+        },
+        greenGem: {
+            value: 0
+        },
+    }
     
     
-    /* 1. function to generate a random number between 19 - 120 and reset users score and score counter.
-    assign new values to each crystal button between 1-12 which will be
-    hidden from the user 
-    until they click it.
-        
-    fucntion that displays the gem points and updates players score when clicked and determines
-    if the player has won or lost and updates wins or loses based on what happened, then restarts
-    
-    */
+    /* A function to create the random number the users need to match, and the
+    values of each gem button */
+    function createRandomNumbers(min, max) {
+        return Math.floor(Math.random() * (max - min) + 1) + min;
+    };
+
+    // function that sets gem values and the random number.
+    function setValues() {
+        // assigning a random value between 1 and 12 to each color gem value.
+        gem.redGem.value = createRandomNumbers(1, 12);
+        gem.blueGem.value = createRandomNumbers(1, 12);
+        gem.yellowGem.value = createRandomNumbers(1, 12);
+        gem.greenGem.value = createRandomNumbers(1, 12);
+
+        //console.log the values for trouble shooting purposes.
+        console.log("Red gem value: " + gem.redGem.value);
+        console.log("Blue gem value: " + gem.blueGem.value);
+        console.log("Yellow gem value: " + gem.yellowGem.value);
+        console.log("Green gem value: " + gem.greenGem.value);
+
+        // assinging a random number between 19 and 120 to the random number variable.
+        randomNumber = createRandomNumbers(19, 120);
+        console.log("Number to hit: " + randomNumber);
+
+        // then we show the random number on the page for the user to see.
+        $("#random-number").text("Number to hit: " + randomNumber);
+
+
+    };
+
 });
