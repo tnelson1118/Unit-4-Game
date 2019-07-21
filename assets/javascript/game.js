cdf33274615f374b51df970dccfed72a09cd2e57
Ty Nelson <tynelson48@yahoo.com>
2019-07-21 10:29:00 -0500

    crystal image files added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+    /* define generic variables.
+    
+    1. function to generate a random number between 19 - 120 and reset users score and score counter.
+    assign new values to each crystal button between 1-12 which will be
+    hidden from the user 
+    until they click it.
+        
+    fucntion that displays the gem points and updates players score when clicked and determines
+    if the player has won or lost and updates wins or loses based on what happened, then restarts
+    
+    */
+});
